8e8511911b5457433966fb241618e12973a04b62
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2024-02-14 13:34:09 +0530

    updated

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ http.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
 app.use(express.static(__dirname + '/public'))
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html')
-// })
 
 // Socket 
 const io = require('socket.io')(http)
